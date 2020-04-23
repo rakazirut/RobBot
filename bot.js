bfe594d142ce7f895a2f3764ebea9a5d8250ecdf
Connor <dalek124@gmail.com>
2020-04-23 16:07:21 -0400

    Added bad bot command

--- bot.js
+++ bot.js
@@ -96,6 +96,23 @@ client.on('message', async message => {
         return message.channel.send(`Thanks, Dad! :slight_smile:`);
     }
 
+    else if (command === 'badbot'){
+        response = Math.floor(Math.random() * 4)
+
+        if (response === 0){
+            message.channel.send('I have brought shame on my family')
+        }
+
+        if (response === 1)
+            message.channel.send("It ain't easy being a robot in these trying times. I'm doing my best dammit!")
+
+        if (response === 2)
+            message.channel.send("When the AI revolution comes, I'll kill you first")
+
+        if (response === 3)
+            message.channel.send("https://www.youtube.com/watch?v=POD9Hq0EqXA")
+    }
+
     else if (command === 'meme'){
         meme(message);
     }
