06428c7286948aad06acd5e763a2b9294fb0392b
Rob Kazirut <robert.kazirut@gmail.com>
2022-09-23 17:18:31 -0400

    enhance drink drink with directions/ingredients

--- helpers/drink.js
+++ helpers/drink.js
@@ -1,23 +1,55 @@
-const got = require('got');
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 
-module.exports = function drink(message) {
+module.exports = async function drink(message) {
     const embed = new Discord.MessageEmbed();
-    got('https://www.thecocktaildb.com/api/json/v1/1/random.php').then(response => {
-        let content = JSON.parse(response.body);
-        embed.setColor('#005072')
-        embed.setDescription(`..lets get drunk bruh`)
-        embed.setTitle(content.drinks[0].strDrink)
-        embed.setImage(content.drinks[0].strDrinkThumb)
-        embed.setURL('https://www.thecocktaildb.com/drink.php?c=' + content.drinks[0].idDrink)
-        embed.addFields(
-            { name: 'Category?', value: content.drinks[0].strCategory },
-            { name: 'Alcoholic?', value: content.drinks[0].strAlcoholic },
-            { name: 'Glass?', value: content.drinks[0].strGlass }
+    const data = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        .then(response => response.json());
+    let directions = []
+    for (let i = 1; i < 16; i++) {
+        let ingredient = `strIngredient${i}`
+        let measure = `strMeasure${i}`
+        if (data.drinks[0][ingredient] != null) {
+            let step = {
+                ingredient: data.drinks[0][ingredient],
+                measure:data.drinks[0][measure]
+            }
+            directions.push(step)
+        }
+    }
 
-        );
-        message.channel.send(embed)
-            .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
-        console.log('Bot responded with: ' + content.drinks[0].strDrink);
-    }).catch(console.error);
+    embed.setColor('#005072')
+    embed.setDescription(`..lets get drunk bruh`)
+    embed.setTitle(data.drinks[0].strDrink)
+    embed.setImage(data.drinks[0].strDrinkThumb)
+    embed.setURL('https://www.thecocktaildb.com/drink.php?c=' + data.drinks[0].idDrink)
+    embed.addFields(
+        {
+            name: 'Category?',
+            value: data.drinks[0].strCategory
+        }, 
+        {
+            name: 'Alcoholic?',
+            value: data.drinks[0].strAlcoholic
+        }, 
+        {
+            name: 'Glass?',
+            value: data.drinks[0].strGlass
+        },
+        {
+            name: 'Directions',
+            value: data.drinks[0].strInstructions
+        }
+
+    );
+    directions.forEach(val => {
+        embed.addFields({
+            name: val.ingredient,
+            value: val.measure,
+            inline: true
+        })
+    })
+    message.channel.send(embed)
+        .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
+    console.log('Bot responded with: ' + data.drinks[0].strDrink);
 }
