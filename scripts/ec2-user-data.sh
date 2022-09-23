b856b1d19bbb7281203baa471ff926128709e990
Rob Kazirut <robert.kazirut@gmail.com>
2022-09-23 16:10:12 -0400

    Update script for using docker on ec2 instance

--- scripts/ec2-user-data.sh
+++ scripts/ec2-user-data.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user
 sudo yum update -y
 
 # Install git, unzip and say yes to any questions
-sudo yum install git unzip -y
+sudo yum install git unzip docker -y
 
 # Clone repo
 git clone https://github.com/rakazirut/RobBot.git
@@ -15,14 +15,13 @@ git clone https://github.com/rakazirut/RobBot.git
 # Install AWS cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/installls
+sudo ./aws/installlsawx
 rm awscliv2.zip
 
-# Install NVM and Node version we want
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash   
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 16.17.0
+#Add group membership for the default ec2-user so you can run all docker commands without using the sudo command
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
 
 # Change pwd to RobBot
 cd RobBot
@@ -30,6 +29,14 @@ cd RobBot
 # Copy auth.json from s3 to ec2
 aws s3 cp s3://rkrobbot/auth.json .
 
-# Install dependancies and run bot
-npm i
-node bot.js
+# Enable docker service at AMI boot time
+sudo systemctl enable docker.service
+
+# Start the Docker service
+sudo systemctl start docker.service
+
+# Build RobBot docker image
+docker build -t robbot .
+
+# Start RobBot docker container
+docker run -d --restart on-failure robbot
